bff7c5a625078d8cca62ce048ea519eb75fc79e5
mustapha <dam1mustapha.hm@gmail.com>
2026-05-20 19:34:34 +0200

    actualizacion

--- Prueba4git/src/gitPrueba4/main.java
+++ Prueba4git/src/gitPrueba4/main.java
@@ -1,8 +1,45 @@
 package gitPrueba4;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class main {
-public main() {
-	// TODO Auto-generated constructor stub
-	
-}
+
+	public static void main(String[] args) {
+
+        int contador = 0;
+
+        try {
+            File archivoEntrada = new File("numeros.txt");
+            Scanner lector = new Scanner(archivoEntrada);
+
+            FileWriter escritor = new FileWriter("pares.txt");
+
+            escritor.write("Números pares encontrados:\n");
+
+            while (lector.hasNextInt()) {
+
+                int numero = lector.nextInt();
+
+                if (numero % 2 == 0) {
+                    escritor.write(numero + "\n");
+                    contador++;
+                }
+              
+            }
+
+            escritor.write("\nTotal de números pares: " + contador);
+
+            lector.close();
+            escritor.close();
+
+            System.out.println("Archivo pares.txt creado correctamente");
+            System.out.println(escritor);
+
+        } catch (IOException e) {
+            System.out.println("Error al trabajar con los archivos");
+        }
+    }
 }
